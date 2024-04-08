37fef8d83f24b19192bf840d169324555356958d
DannerManner24k <dantheawesime24@gmail.com>
2024-04-08 11:53:42 +0200

    few changes

--- GameEngine/src/main/java/Core/GameScreen.java
+++ GameEngine/src/main/java/Core/GameScreen.java
@@ -16,44 +16,44 @@ import static Helper.Constants.PPM;
 
 public class GameScreen extends ScreenAdapter {
 
-    private OrthographicCamera camera;
-    private SpriteBatch batch;
-    private World world;
-    private Box2DDebugRenderer box2DDebugRenderer;
+    private OrthographicCamera camera; // Camera to render the game
+    private SpriteBatch batch; // Batch to render the game
+    private World world; // Box2D world
+    private Box2DDebugRenderer box2DDebugRenderer; // Box2D debug renderer
 
     public GameScreen(OrthographicCamera camera) {
-        this.camera = camera;
-        this.batch = new SpriteBatch();
-        this.world = new World(new Vector2(0,0), false);
-        this.box2DDebugRenderer = new Box2DDebugRenderer();
+        this.camera = camera; // Set the camera
+        this.batch = new SpriteBatch();// Set the batch
+        this.world = new World(new Vector2(0,0), false); // Set the world
+        this.box2DDebugRenderer = new Box2DDebugRenderer(); // Set the debug renderer
     }
 
     private void update() {
-        world.step(1/60f, 6, 2);
-        cameraUpdate();
+        world.step(1/60f, 6, 2); // Update the world
+        cameraUpdate(); // Update the camera
 
-        batch.setProjectionMatrix(camera.combined);
+        batch.setProjectionMatrix(camera.combined); // Set the batch projection matrix
 
-        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
-            Gdx.app.exit();
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) { // If the escape key is pressed
+            Gdx.app.exit(); // Exit the game
         }
     }
 
     private void cameraUpdate() {
-        camera.position.set(new Vector3(0,0,0));
-        camera.update();
+        camera.position.set(new Vector3(0,0,0)); // Set the camera position
+        camera.update();// Update the camera
     }
 
     @Override
     public void render(float delta) {
-        this.update();
+        this.update();// Update the game
 
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(0, 0, 0, 1); // Set the clear color
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear the screen
 
-        batch.begin();
+        batch.begin(); // Begin the batch
 
-        batch.end();
-        box2DDebugRenderer.render(world, camera.combined.scl(PPM));
+        batch.end();// End the batch
+        box2DDebugRenderer.render(world, camera.combined.scl(PPM));// Render the Box2D world
     }
 }
