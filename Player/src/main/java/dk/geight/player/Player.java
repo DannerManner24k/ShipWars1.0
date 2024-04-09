0d76c0f6e1e8bf53670dcd709cc43e3b1daa438f
DannerManner24k <dantheawesime24@gmail.com>
2024-04-09 02:27:09 +0200

    safe commit

---
+++ Player/src/main/java/dk/geight/player/Player.java
@@ -0,0 +1,11 @@
+package dk.geight.player;
+
+import dk.geight.character.Character;
+
+public class Player extends Character{
+
+
+    public Player(int maxHealth, float x, float y) {
+        super(maxHealth, x, y);
+    }
+}
